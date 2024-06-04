f0965e73a34a2ec52408a6fba5519c649af9fed2
Ellen <ellen31725@gmail.com>
2024-06-04 20:53:59 +0800

    fix: modify the nearBottom to load new page data

--- static/js/home.js
+++ static/js/home.js
@@ -45,7 +45,7 @@ function handleSearchInput(keyword) {
 function setupInfiniteScroll() {
     function handleScroll() {
        const distanceToBottom = document.documentElement.scrollHeight - window.innerHeight - window.scrollY;
-       const nearBottom = distanceToBottom < 150;
+       const nearBottom = distanceToBottom < 180;
 
         if (nearBottom && !isLoading && nextPage !== null) {
             fetchAttractions(currentKeyword);
